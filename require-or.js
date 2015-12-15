b8ed877fb0b2ddea6b3dec67768c49240ebace6d
Gustav Nikolaj Olsen <gno@one.com>
2015-12-15 23:03:03 +0100

    first stab

--- require-or.js
+++ require-or.js
@@ -1,3 +1,29 @@
-module.exports = function (id, callback) {
+function requireOrFactory (require) {
+    return function (id, callback) {
+        if (callback && typeof callback !== 'function') {
+            throw new Error('Second argument must be a function.');
+        }
+        try {
+            return require(id);
+        } catch (e) {
+            var result;
+            if (callback) {
+                result = callback();
+            }
+            return result;
+        }
+    };
+}
 
+module.exports = function (id, callback) {
+    if (arguments.length === 1 && typeof arguments[0] === 'function') {
+        return requireOrFactory(arguments[0]);
+    }
+    if (id.substr(0, 1) === '.') {
+        throw new Error(
+            'Relative paths are not supported, when require has not been passed to require-or.'
+        );
+    }
+
+    return requireOrFactory(require)(id, callback);
 };
--- test/require-or.spec.js
+++ test/require-or.spec.js
@@ -1,8 +1,64 @@
 var expect = require('unexpected');
-var requireOr = require('../');
+var requireOrFactory = require('../');
+
+function mockNodeModules (path) {
+    process.env.NODE_PATH = path;
+    require('module').Module._initPaths();
+    return function revertMockNodeModules () {
+        process.env.NODE_PATH = undefined;
+        require('module').Module._initPaths();
+    };
+}
 
 describe('require, or...', function () {
-    it('exports a function', function () {
-        return expect(requireOr, 'to be a function');
+    describe('with require passed', function () {
+        var requireOr;
+        before(function () {
+            requireOr = requireOrFactory(require);
+        });
+        it('should work like require', function () {
+            var someModule = require('./fixtures/some-module');
+            return expect(requireOr('./fixtures/some-module'), 'to equal', someModule);
+        });
+        it('should call the callback when the require fails', function () {
+            return expect(function (cb) {
+                requireOr('./missing/module', cb);
+            }, 'to call the callback');
+        });
+        it('should not call the callback, when the require succeeds', function () {
+            return expect(function () {
+                requireOr('./fixtures/some-module', function () {
+                    throw new Error('Callback was called');
+                });
+            }, 'not to throw');
+        });
+        it('should not return the value from the callback', function () {
+            var someModule = requireOr('./missing/module', function () {
+                return { module: 'some-other-module' };
+            });
+            return expect(someModule, 'to have property', 'module', 'some-other-module');
+        });
+        it('should work when non-relative paths', function () {
+            var someModule = require('./fixtures/some-module');
+            var mockPath = require('path').resolve(__dirname, 'fixtures');
+            var undoMock = mockNodeModules(mockPath);
+
+            return expect(requireOr('some-module'), 'to equal', someModule).finally(undoMock);
+        });
+    });
+    describe('without require passed', function () {
+        var requireOr = requireOrFactory;
+        it('should throw when using relative paths', function () {
+            return expect(function () {
+                requireOr('./fixtures/some-module');
+            }, 'to throw', /Relative paths are not supported/);
+        });
+        it('should work when non-relative paths', function () {
+            var someModule = require('./fixtures/some-module');
+            var mockPath = require('path').resolve(__dirname, 'fixtures');
+            var undoMock = mockNodeModules(mockPath);
+
+            return expect(requireOr('some-module'), 'to equal', someModule).finally(undoMock);
+        });
     });
 });
